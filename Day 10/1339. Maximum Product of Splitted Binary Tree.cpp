b771252307040fe04f8df7968638845aa4189083
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-12-10 18:54:52 +0530

    Add files via upload

---
+++ Day 10/1339. Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    long long int MOD = 1e9+7;
+    long long totalSum =0, ans=0;
+    int find(TreeNode*root)
+    {
+    if(root == NULL)
+     {
+        return 0;
+     }
+     long long sum =find(root->left) + find(root->right) + root->val;
+     ans = max(ans,(totalSum-sum)*sum );
+     return sum;
+    }
+    int maxProduct(TreeNode* root) {
+     totalSum = find(root);
+     find(root);
+     return ans%MOD;
+     
+    }
+};
