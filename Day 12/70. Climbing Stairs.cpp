4ec444b3d88628b4220aad0d79e5afffbb47c938
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-12-12 10:08:50 +0530

    Add files via upload

---
+++ Day 12/70. Climbing Stairs.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/climbing-stairs/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+2, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
