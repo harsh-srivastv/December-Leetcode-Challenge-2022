3b7256792a3e6419350ea4b745305554ac3ad1b6
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-12-25 17:36:17 +0530

    Add files via upload

---
+++ Day 25/2389. Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/longest-subsequence-with-limited-sum/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        vector<int> ans;
+        
+        for(auto tar : queries)
+        {
+            int cnt = 0;
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] <= tar)
+                {
+                    tar = tar-nums[i];
+                    cnt++;
+                }
+                else
+                    break;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
