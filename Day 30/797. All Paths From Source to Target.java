f1ba8caa087414e52337cce727d8bba5768b4f21
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-12-30 19:15:51 +0530

    Add files via upload

---
+++ Day 30/797. All Paths From Source to Target.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/all-paths-from-source-to-target/description/
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    path.add(0);
+    deepSearch(graph, 0, path, res);
+
+    return res;
+}
+
+public void deepSearch(int[][] graph, int index, List<Integer> path, List<List<Integer>> res) {
+    if (index + 1 == graph.length) {
+        res.add(new ArrayList<>(path));
+        return;
+    }
+    for (int node : graph[index]) {
+        path.add(node);
+        deepSearch(graph, node, path, res);
+        path.remove(path.size() - 1);
+    }
+}
+}
